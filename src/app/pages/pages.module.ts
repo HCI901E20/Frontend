d4aff0b9771095c64e3e6bbcbf8d54198d08068e
Jonas <grann.alsen@gmail.com>
2020-09-21 11:22:26 +0200

    Setup page/component structure

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { PagesRoutingModule } from './pages-routing.module';
+import { ComponentsModule } from '../components/components.module';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    ComponentsModule
+  ]
+})
+export class PagesModule { }
