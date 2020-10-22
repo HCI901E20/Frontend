912d2aba7f2816e6ef3f74647eb7a210b7b1d8a7
Jonas <grann.alsen@gmail.com>
2020-10-22 14:21:44 +0200

    fix: Add placeholder variables to environment.ts to allow CI to build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  api : {
+    baseUrl: 'https://someurlhere:420/api',
+    apiKeys: {
+      googleMaps: 'APIKEYHERE',
+      openWeatherMap: 'APIKEYHERE'
+    }
+  },
+  production: false,
 };
 
 /*
