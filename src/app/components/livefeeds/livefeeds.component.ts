0b0139a7a00f75d600ccc101d4c5fd135d4b7d04
Alechj <alec@hotmail.dk>
2021-02-18 11:16:22 +0100

    chore: created components

---
+++ src/app/components/livefeeds/livefeeds.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livefeeds',
+  templateUrl: './livefeeds.component.html',
+  styleUrls: ['./livefeeds.component.scss']
+})
+export class LivefeedsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
