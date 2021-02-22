6ca494d2e3fc93fc104401e6bec862780d7a3e57
Lukas Rønsholt <lukasronsholt@gmail.com>
2021-02-22 10:19:19 +0100

    feat: Drone & Boat Orientation, angle based

--- src/app/services/boat.service.ts
+++ src/app/services/boat.service.ts
@@ -9,6 +9,9 @@ export class BoatService {
   private boatPositionSub: BehaviorSubject<Position> = new BehaviorSubject<Position>(this.getPosition());
   public BoatPosition: Observable<Position> = this.boatPositionSub.asObservable();
 
+  private boatOrientationSub: BehaviorSubject<number> = new BehaviorSubject<number>(130);
+  public BoatOrientation: Observable<number> = this.boatOrientationSub.asObservable();
+
   constructor() {
     interval(1000).subscribe(() => this.boatPositionSub.next(this.getPosition()))
   }
@@ -16,4 +19,5 @@ export class BoatService {
   private getPosition(): Position {
     return Object.assign(new Position(), {lat: 57.052078, lng: 9.929658})
   }
+  
 }
