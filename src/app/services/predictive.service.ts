a3f58c7400d55880e0e97e84a6e392cce171f48f
Luke <Lukasronsholt@gmail.com>
2021-03-18 10:38:44 +0100

    Update predictive.service.ts

--- src/app/services/predictive.service.ts
+++ src/app/services/predictive.service.ts
@@ -7,7 +7,7 @@ import { FeedsService } from './feeds.service';
 })
 export class PredictiveService {
   private TriggerSub: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  private DataSub: BehaviorSubject<number> = new BehaviorSubject<number>(1);
+  private DataSub: BehaviorSubject<number> = new BehaviorSubject<number>(NaN);
   private ShowInfoCardSub: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   private IntrusiveSub: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
