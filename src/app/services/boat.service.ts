adeef6799c8a9ace057e94979120d52ce5e06883
Lukas Rønsholt <lukasronsholt@gmail.com>
2021-03-31 10:24:09 +0200

    feat: Read boat position from db

--- src/app/services/boat.service.ts
+++ src/app/services/boat.service.ts
@@ -1,46 +1,70 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { BehaviorSubject, interval, Observable, Subscription } from 'rxjs';
 import { take } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Boat } from '../models/boat.model';
 import { Position } from '../models/position.model';
+import { ApiBaseService } from './api-base.service';
 import { MapService } from './map.service';
 import { TickService } from './tick.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BoatService {
-  private position: Position = {lat: 57.049356121182555, lng: 9.940183888977057};
+export class BoatService extends ApiBaseService<Boat, string> {
   private ms: number = 10;
 
-  private boatPositionSub: BehaviorSubject<Position> = new BehaviorSubject<Position>(this.getPosition());
+  private boatPositionSub: BehaviorSubject<Position> = new BehaviorSubject<Position>(null);
   public BoatPosition: Observable<Position> = this.boatPositionSub.asObservable();
 
-  private boatOrientationSub: BehaviorSubject<number> = new BehaviorSubject<number>(130);
+  private boatOrientationSub: BehaviorSubject<number> = new BehaviorSubject<number>(null);
   public BoatOrientation: Observable<number> = this.boatOrientationSub.asObservable();
 
   private movementSub: Subscription;
 
 
   constructor(private mapService: MapService,
-    private tickService: TickService) {
-    this.tickService.UpdateClock.subscribe(() => this.boatPositionSub.next(this.getPosition()));
+    private tickService: TickService,
+    private http: HttpClient,
+    private toast: ToastrService) {
+      super(`${environment.api.baseUrl}/boats`,http,toast)
+    this.tickService.UpdateClock.subscribe(() => this.getPosition());
   }
 
-  private getPosition(): Position {
-    return this.position;
+  private getPosition(): void {
+    this.get('1').subscribe({
+      next: (boat: Boat) => {
+        this.boatPositionSub.next({lat: boat.currentPosition.latitude, lng: boat.currentPosition.longitude});
+        this.boatOrientationSub.next(boat.currentPosition.bearing - 90);
+      }
+    });
   }
 
   public MoveTo(lat: number, lng: number): void {
     this.movementSub?.unsubscribe();
-    const distance = this.mapService.calculateDistance(this.position.lat, this.position.lng, lat, lng);
-    const steps = distance / this.ms;    
+    console.log('click');
+
+    let position = this.boatPositionSub.value;
+    const distance = this.mapService.calculateDistance(position.lat, position.lng, lat, lng);
+    const steps = distance / this.ms;
 
     this.movementSub = this.tickService.UpdateClock.pipe(take(steps)).subscribe(() => {
-      const bearing = this.mapService.calculateBearing(this.position.lat, this.position.lng, lat, lng);
-      this.boatOrientationSub.next(bearing - 90);
+      console.log('update');
 
-      this.position = this.mapService.getCoordinate(this.position.lat, this.position.lng, bearing, this.ms);     
+      position = this.boatPositionSub.value;
+      const bearing = this.mapService.calculateBearing(position.lat, position.lng, lat, lng);
+      const newPosition = this.mapService.getCoordinate(position.lat, position.lng, bearing, this.ms);
+
+
+      this.put('1', {id: 1, currentPosition: {
+        latitude: newPosition.lat,
+        longitude: newPosition.lng,
+        altitude: 0,
+        bearing : bearing,
+      }}).pipe(take(1)).subscribe()
     });
   }
-  
+
 }
